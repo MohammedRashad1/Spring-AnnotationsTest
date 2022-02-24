d1a22d0717443382c4995d18a813d827ccb5e2f1
MohammedRashad1 <A-10563@ibsplc.com>
2022-02-24 19:46:14 +0530

    first commit

---
+++ src/main/java/com/ibs/training/SpringAnnotationsAssignment/App.java
@@ -0,0 +1,16 @@
+package com.ibs.training.SpringAnnotationsAssignment;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+        ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+        JDBCConnection jdbc = context.getBean("jdbc", JDBCConnection.class);
+        jdbc.display();
+        jdbc.getJDBCConnection();
+       
+    }
+}
